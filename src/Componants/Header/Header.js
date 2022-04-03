8ab78a35fab198bcf0fdf5d98648816666029568
Al-aminhossain1 <md.al.amihossain1785@gmail.com>
2022-04-03 23:55:43 +0600

    header added

---
+++ src/Componants/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <a href="HOME">HOME</a>
+                <a href="REVIEW">REVIEW</a>
+                <a href="">DASHBOARD</a>
+                <a href="">ABOUT</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
